f9652321382521a0c367f0eb44201e34f7f60980
brandolkuete <brandolkuete95@gmail.com>
2020-12-22 06:41:58 +0100

    Updates

---
+++ precache-manifest.510d04fbd65552f9711eb724ef430e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2deeb152602fa5e333bee0558369a842",
+    "url": "/student-react/index.html"
+  },
+  {
+    "revision": "763354a999a6ddfbe788",
+    "url": "/student-react/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a62273555362ac8dccd7",
+    "url": "/student-react/static/css/main.cc6d6b7d.chunk.css"
+  },
+  {
+    "revision": "763354a999a6ddfbe788",
+    "url": "/student-react/static/js/2.f480d01f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/student-react/static/js/2.f480d01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62273555362ac8dccd7",
+    "url": "/student-react/static/js/main.1db7c412.chunk.js"
+  },
+  {
+    "revision": "12a97b96f36f4e5fc41f",
+    "url": "/student-react/static/js/runtime-main.ab23637f.js"
+  }
+]);
